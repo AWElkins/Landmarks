9bea327a1285532b6db1cbb2b1a2a8f781109d1d
Aaron Elkins <akatoll@hotmail.com>
2022-06-18 18:00:37 -0700

    Basic SwiftUI app

---
+++ Landmarks/Views/Bagde.swift
@@ -0,0 +1,20 @@
+//
+//  Bagde.swift
+//  Landmarks
+//
+//  Created by Aaron Elkins on 6/18/22.
+//
+
+import SwiftUI
+
+struct Bagde: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Bagde_Previews: PreviewProvider {
+    static var previews: some View {
+        Bagde()
+    }
+}
---
+++ Landmarks/Views/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Landmarks
+//
+//  Created by Aaron Elkins on 6/18/22.
+//
+
+import Foundation
